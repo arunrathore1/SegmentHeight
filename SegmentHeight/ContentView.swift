203918a087852c7075b9cb5ec4672443ea556a21
Arun Rathore <arathore@nowoptics.com>
2024-04-09 16:10:18 +0530

    Initial Commit

---
+++ SegmentHeight/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  SegmentHeight
+//
+//  Created by Arun Rathore on 09/04/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ SegmentHeight/SegmentHeightApp.swift
@@ -0,0 +1,17 @@
+//
+//  SegmentHeightApp.swift
+//  SegmentHeight
+//
+//  Created by Arun Rathore on 09/04/24.
+//
+
+import SwiftUI
+
+@main
+struct SegmentHeightApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
